a8a395af4d7a544d38c0b4c31b92de224c7aed3f
Edward Amor <edward.amor3@gmail.com>
2022-09-08 06:18:40 +0000

    fix: outline encoder class methods

--- src/eth/codecs/abi/encoder.py
+++ src/eth/codecs/abi/encoder.py
@@ -1,11 +1,43 @@
+import decimal
+from collections.abc import Sequence
 from typing import Any
 
 from eth.codecs.abi import datatypes
 
 
 class Encoder:
-    def __init__(self, datatype: datatypes.DataType):
-        self.datatype = datatypes
+    @classmethod
+    def encode(cls, datatype: datatypes.DataType, value: Any) -> bytes:
+        return datatype.accept(cls, value)
 
-    def encode(self, value: Any) -> bytes:
-        return self.datatype.accept(self, value)
+    @staticmethod
+    def visit_Address(_, value: str) -> bytes:
+        pass
+
+    @classmethod
+    def visit_Array(cls, dt: datatypes.Array, value: Sequence) -> bytes:
+        pass
+
+    @staticmethod
+    def visit_Bool(_, value: bool) -> bytes:
+        pass
+
+    @staticmethod
+    def visit_Bytes(dt: datatypes.Bytes, value: bytes) -> bytes:
+        pass
+
+    @staticmethod
+    def visit_Fixed(dt: datatypes.Fixed, value: decimal.Decimal) -> bytes:
+        pass
+
+    @staticmethod
+    def visit_Integer(dt: datatypes.Integer, value: int) -> bytes:
+        pass
+
+    @classmethod
+    def visit_String(cls, _, value: str) -> bytes:
+        pass
+
+    @classmethod
+    def visit_Tuple(cls, dt: datatypes.Tuple, value: Sequence) -> bytes:
+        pass
