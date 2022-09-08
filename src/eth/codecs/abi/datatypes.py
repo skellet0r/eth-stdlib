bb7037a7c46162b16acc5ab33de7c87b9c4dc73e
Edward Amor <edward.amor3@gmail.com>
2022-09-08 06:06:00 +0000

    fix: add is_dynamic property to potentially dynamic datatypes

--- src/eth/codecs/abi/datatypes.py
+++ src/eth/codecs/abi/datatypes.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import cached_property
 from typing import Any
 
 
@@ -18,6 +19,10 @@ class Array(DataType):
     subtype: DataType
     size: int
 
+    @cached_property
+    def is_dynamic(self) -> bool:
+        return self.size == -1 or getattr(self.subtype, "is_dynamic", False)
+
 
 @dataclass(init=False, eq=False, slots=True)
 class Bool:
@@ -27,6 +32,10 @@ class Bool:
 @dataclass(eq=False, slots=True)
 class Bytes(DataType):
     size: int
+
+    @cached_property
+    def is_dynamic(self) -> bool:
+        return self.size == -1
 
 
 @dataclass(eq=False, slots=True)
@@ -44,9 +53,15 @@ class Integer(DataType):
 
 @dataclass(init=False, eq=False, slots=True)
 class String(DataType):
-    ...
+    @cached_property
+    def is_dynamic(self) -> bool:
+        return True
 
 
 @dataclass(eq=False, slots=True)
 class Tuple(DataType):
     components: list[DataType]
+
+    @cached_property
+    def is_dynamic(self):
+        return any((getattr(elem, "is_dynamic", False) for elem in self.components))
