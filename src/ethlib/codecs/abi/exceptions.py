83336ea3c3189dce073d160f52924ac30e193609
Edward Amor <edward.amor3@gmail.com>
2022-09-08 00:42:23 +0000

    fix: add ParseError class

--- src/ethlib/codecs/abi/exceptions.py
+++ src/ethlib/codecs/abi/exceptions.py
@@ -1,4 +1,12 @@
 class ABIError(Exception):
-    def __init__(self, msg, *args, **kwargs):
+    ...
+
+
+class ParseError(ABIError):
+    def __init__(self, where: str, msg: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.where = where
         self.msg = msg
+
+    def __str__(self) -> str:
+        return f"Error at {self.where!r} - {self.msg}"
