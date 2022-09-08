6daa11009f66058267cf99d713983e58ef24c3d1
Edward Amor <edward.amor3@gmail.com>
2022-09-08 17:29:52 +0000

    fix: implement encoding address values

--- src/eth/codecs/abi/encoder.py
+++ src/eth/codecs/abi/encoder.py
@@ -3,6 +3,7 @@ from collections.abc import Sequence
 from typing import Any
 
 from eth.codecs.abi import datatypes
+from eth.codecs.abi.exceptions import EncodeError
 
 
 class Encoder:
@@ -12,7 +13,20 @@ class Encoder:
 
     @staticmethod
     def visit_Address(_, value: str) -> bytes:
-        pass
+        try:
+            bval = bytes.fromhex(value.removeprefix("0x"))
+            assert len(bval) == 20
+            return bval.rjust(32, b"\x00")
+        except (AttributeError, TypeError) as e:
+            # AttributeError - if `value` does not have `removeprefix` method (only bytes | str)
+            # TypeError - if `value` is not a `str` instance (if value == bytes)
+            raise EncodeError("address", value, "Value is not an instance of type 'str'") from e
+        except ValueError as e:
+            # ValueError - if value contains non-hexadecimal characters (e.g "-0x...", "0xSJ32...")
+            raise EncodeError("address", value, "Value contains non-hexadecimal number(s)") from e
+        except AssertionError as e:
+            # AssertionError - if length of the bytes value is not 20
+            raise EncodeError("address", value, "Value is not 20 bytes") from e
 
     @classmethod
     def visit_Array(cls, dt: datatypes.Array, value: Sequence) -> bytes:
