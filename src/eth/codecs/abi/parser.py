784c2bb303b10651da10dcdfe3a548a6fc054785
Edward Amor <edward.amor3@gmail.com>
2022-09-10 07:59:24 +0000

    chore: fix boiler plate license in source files

--- src/eth/codecs/abi/parser.py
+++ src/eth/codecs/abi/parser.py
@@ -1,12 +1,12 @@
-"""This file is part of eth-stdlib.
+"""This file is part of the eth-stdlib library.
 Copyright (C) 2022  Edward Amor
 
-eth-stdlib is free software: you can redistribute it and/or modify
+The eth-stdlib library is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-eth-stdlib is distributed in the hope that it will be useful,
+The eth-stdlib library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Lesser General Public License for more details.
