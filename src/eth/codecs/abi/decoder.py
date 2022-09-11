c14fb4eceeb8b127c14969bed3bbf204cdd4084a
Edward Amor <edward.amor3@gmail.com>
2022-09-11 16:38:20 +0000

    fix: outline decoder class

--- src/eth/codecs/abi/decoder.py
+++ src/eth/codecs/abi/decoder.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+import decimal
 from typing import Any
 
 from eth.codecs.abi import nodes
@@ -29,3 +30,35 @@ class Decoder:
             param, typ = e.args[0]
             raise TypeError(f"Received invalid type {typ!r} for parameter {param!r}")
         return node.accept(cls, value)
+
+    @staticmethod
+    def visit_Address(node: nodes.Address, value: bytes) -> str:
+        pass
+
+    @classmethod
+    def visit_Array(cls, node: nodes.Array, value: bytes) -> list[Any]:
+        pass
+
+    @staticmethod
+    def visit_Bool(node: nodes.Bool, value: bytes) -> bool:
+        pass
+
+    @staticmethod
+    def visit_Bytes(node: nodes.Bytes, value: bytes) -> bytes:
+        pass
+
+    @staticmethod
+    def visit_Fixed(node: nodes.Fixed, value: bytes) -> decimal.Decimal:
+        pass
+
+    @staticmethod
+    def visit_Integer(node: nodes.Integer, value: bytes) -> int:
+        pass
+
+    @staticmethod
+    def visit_String(node: nodes.String, value: bytes) -> str:
+        pass
+
+    @staticmethod
+    def visit_Tuple(node: nodes.Tuple, value: bytes) -> list[Any]:
+        pass
