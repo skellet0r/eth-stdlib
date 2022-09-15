904d838f534ce0a42a0c0351b42c8f7bc0d9744d
Edward Amor <edward.amor3@gmail.com>
2022-09-15 15:00:11 +0000

    fix: node strategies reduce sized array|tuple upper bounds

--- tests/strategies/abi/nodes.py
+++ tests/strategies/abi/nodes.py
@@ -2,6 +2,13 @@ from hypothesis import strategies as st
 
 from eth.codecs.abi import nodes
 
+# limit max size of tuples and arrays, needs to be a reasonable number otherwise
+# value generation will bork for exceedingly large numbers
+MAX_SIZE = 10
+# limit the depth of recursive array and tuple strategies
+MAX_DEPTH = 3
+
+
 # helper strategies
 bits = st.sampled_from(range(8, 264, 8))
 
@@ -19,27 +26,29 @@ Atomic = st.one_of(Address, Bool, Bytes, Fixed, Integer, String)
 
 @st.deferred
 def Array():
-    # top-level will always be an array
-    size = st.just(-1) | st.integers(1)
+    size = st.just(-1) | st.integers(1, MAX_SIZE)
     array = st.builds(nodes.Array, Atomic | Tuple, size)
-    return st.recursive(array, lambda s: st.builds(nodes.Array, s, size), max_leaves=3)
+    # top-level will always be an array
+    return st.recursive(array, lambda s: st.builds(nodes.Array, s, size), max_leaves=MAX_DEPTH)
 
 
 @st.deferred
 def Tuple():
-    # top-level will always be a tuple
-    components = st.lists(Atomic | Array, min_size=1)
+    components = st.lists(Atomic | Array, min_size=1, max_size=MAX_SIZE)
     tuple_ = st.builds(nodes.Tuple, components)
+    # top-level will always be a tuple
     return st.recursive(
-        tuple_, lambda s: st.builds(nodes.Tuple, st.lists(s, min_size=1)), max_leaves=3
+        tuple_,
+        lambda s: st.builds(nodes.Tuple, st.lists(s, min_size=1, max_size=MAX_SIZE)),
+        max_leaves=MAX_DEPTH,
     )
 
 
 # recursive strategy for composite types - top-level can be any valid abi type
 def extend(base: st.SearchStrategy) -> st.SearchStrategy:
-    arrays = st.builds(nodes.Array, base, st.just(-1) | st.integers(1))
-    tuples = st.builds(nodes.Tuple, st.lists(base, min_size=1))
+    arrays = st.builds(nodes.Array, base, st.just(-1) | st.integers(1, MAX_SIZE))
+    tuples = st.builds(nodes.Tuple, st.lists(base, min_size=1, max_size=MAX_SIZE))
     return arrays | tuples
 
 
-Composite = st.recursive(Atomic, extend, max_leaves=3)
+Composite = st.recursive(Atomic, extend, max_leaves=MAX_DEPTH)
