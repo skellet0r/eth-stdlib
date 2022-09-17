38de737535fdc3c5fe81eb08cbbae5cfaad23dc9
Edward Amor <edward.amor3@gmail.com>
2022-09-17 20:39:46 +0000

    Revert "chore: rm dev hypothesis profile"
    This reverts commit 823524c841dd96dd37d393d54c55f6648c05cf8b.

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+from hypothesis import settings
+
+# modifies environment, loading env vars from '.env'
+load_dotenv()
+
+# register profile for local development
+settings.register_profile("dev", max_examples=50)
+
+# use the default profile if an env var is not set
+settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
