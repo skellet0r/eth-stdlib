0839e857c1a42e3c27fd38b865f3c35462890d0b
Edward Amor <edward.amor3@gmail.com>
2022-10-07 17:18:07 -0400

    docs: change theme to alabaster

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "nature"
+html_theme = "alabaster"
 html_static_path = ["_static"]
 
 # -- autodoc extension configuration ---------------------------------------------------
