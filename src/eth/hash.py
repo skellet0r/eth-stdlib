98fe23663bd8216cfcb7b4c2c81e6b87a14ad45f
Edward Amor <edward.amor3@gmail.com>
2022-10-07 12:21:23 -0400

    fix(docs): keccak256 docstring

--- src/eth/hash.py
+++ src/eth/hash.py
@@ -2,4 +2,12 @@ import sha3
 
 
 def keccak256(data: bytes) -> bytes:
+    """Ethereum keccak256 hash function.
+
+    Parameters:
+        data: The data to hash.
+
+    Returns:
+        The hash digest.
+    """
     return sha3.keccak_256(data).digest()
