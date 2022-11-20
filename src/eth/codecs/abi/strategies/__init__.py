5e853176f64c6d6c34a2450ff323fb9cb690633b
Edward Amor <edward.amor3@gmail.com>
2022-11-20 02:09:40 -0500

    fix: add schema_and_value strategy

--- src/eth/codecs/abi/strategies/__init__.py
+++ src/eth/codecs/abi/strategies/__init__.py
@@ -20,3 +20,14 @@ def value(draw: st.DrawFn, schema: str) -> Any:
         A valid ABI encodable value for the given type string.
     """
     return draw(StrategyMaker.make_strategy(Parser.parse(schema)))
+
+
+@st.composite
+def schema_and_value(draw: st.DrawFn) -> tuple[str, Any]:
+    """Generate a valid ABI schema and an encodable value for it.
+
+    Returns:
+        A tuple containing the valid ABI type string and the encodable value for the type string.
+    """
+    typestr = draw(schema)
+    return (typestr, draw(value(typestr)))
