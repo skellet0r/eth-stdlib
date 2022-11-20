57d27cd3714568c68de95a50f07539573d448660
Edward Amor <edward.amor3@gmail.com>
2022-11-20 02:00:11 -0500

    fix: add abi value strategy

--- src/eth/codecs/abi/strategies/__init__.py
+++ src/eth/codecs/abi/strategies/__init__.py
@@ -1,5 +1,22 @@
+from typing import Any
+
 import hypothesis.strategies as st
 
+from eth.codecs.abi.parser import Parser
 from eth.codecs.abi.strategies.nodes import Node as st_node
+from eth.codecs.abi.strategies.values import StrategyMaker
 
 schema = st.builds(str, st_node)
+
+
+@st.composite
+def value(draw: st.DrawFn, schema: str) -> Any:
+    """Generate a valid ABI encodable value for a given type string.
+
+    Parameters:
+        schema: A valid ABI type string.
+
+    Returns:
+        A valid ABI encodable value for the given type string.
+    """
+    return draw(StrategyMaker.make_strategy(Parser.parse(schema)))
